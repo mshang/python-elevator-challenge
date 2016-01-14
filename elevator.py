343d4a81da84b90b25d344f423d24efcbbaab947
Miles Shang <mail@mshang.ca>
2016-01-13 20:55:26 -0600

    M	elevator.py

--- elevator.py
+++ elevator.py
@@ -2,61 +2,72 @@ UP = 1
 DOWN = 2
 FLOOR_COUNT = 6
 
+import operator
+
 class ElevatorLogic(object):
-    """
-    An incorrect implementation. Can you make it pass all the tests?
-
-    Fix the methods below to implement the correct logic for elevators.
-    The tests are integrated into `README.md`. To run the tests:
-    $ python -m doctest -v README.md
-
-    To learn when each method is called, read its docstring.
-    To interact with the world, you can get the current floor from the
-    `current_floor` property of the `callbacks` object, and you can move the
-    elevator by setting the `motor_direction` property. See below for how this is done.
-    """
-
     def __init__(self):
-        # Feel free to add any instance variables you want.
-        self.destination_floor = None
+        self.requested_floors = {UP: set([]), DOWN: set([])}
+        self.selected_floors = set([])
+        self.notional_direction = None
         self.callbacks = None
 
     def on_called(self, floor, direction):
-        """
-        This is called when somebody presses the up or down button to call the elevator.
-        This could happen at any time, whether or not the elevator is moving.
-        The elevator could be requested at any floor at any time, going in either direction.
-
-        floor: the floor that the elevator is being called to
-        direction: the direction the caller wants to go, up or down
-        """
-        self.destination_floor = floor
+        self.requested_floors[direction].add(floor)
+        self._set_notional_direction_for_idle()
 
     def on_floor_selected(self, floor):
-        """
-        This is called when somebody on the elevator chooses a floor.
-        This could happen at any time, whether or not the elevator is moving.
-        Any floor could be requested at any time.
+        if floor >= self.callbacks.current_floor and self.notional_direction == DOWN: return
+        if floor <= self.callbacks.current_floor and self.notional_direction == UP: return
+        self.selected_floors.add(floor)
+        self._set_notional_direction_for_idle()
 
-        floor: the floor that was requested
-        """
-        self.destination_floor = floor
+    def _end_of_the_line(self, direction):
+        f = max if direction == UP else min
+        s = self.selected_floors.union(self.requested_floors[UP].union(self.requested_floors[DOWN]))
+        if len(s) == 0: return None
+        return f(s)
+
+    def _set_notional_direction_for_requests(self):
+        notional_direction = self.notional_direction
+        opposite = UP if notional_direction == DOWN else DOWN
+        self.notional_direction = None
+        if self.callbacks.current_floor in self.requested_floors[notional_direction]:
+            self.notional_direction = notional_direction
+        elif self.callbacks.current_floor in self.requested_floors[opposite]:
+            self.notional_direction = opposite
+        self._set_notional_direction_for_idle()
+
+    def _set_notional_direction_for_idle(self):
+        if self.notional_direction is None:
+            if self._should_move(UP): self.notional_direction = UP
+            elif self._should_move(DOWN): self.notional_direction = DOWN
 
     def on_floor_changed(self):
-        """
-        This lets you know that the elevator has moved one floor up or down.
-        You should decide whether or not you want to stop the elevator.
-        """
-        if self.destination_floor == self.callbacks.current_floor:
-            self.callbacks.motor_direction = None
+        if self.callbacks.current_floor == self._end_of_the_line(self.notional_direction):
+            self._set_notional_direction_for_requests()
+        elif self.callbacks.current_floor in self.selected_floors.union(
+            self.requested_floors[self.notional_direction]
+        ): pass
+        else: return
+
+        self.callbacks.motor_direction = None
+
+    def _should_move(self, direction):
+        c = operator.lt if direction == UP else operator.gt
+        end_of_the_line = self._end_of_the_line(direction)
+        if end_of_the_line is None: return False
+        return c(self.callbacks.current_floor, self._end_of_the_line(direction))
 
     def on_ready(self):
-        """
-        This is called when the elevator is ready to go.
-        Maybe passengers have embarked and disembarked. The doors are closed,
-        time to actually move, if necessary.
-        """
-        if self.destination_floor > self.callbacks.current_floor:
-            self.callbacks.motor_direction = UP
-        elif self.destination_floor < self.callbacks.current_floor:
-            self.callbacks.motor_direction = DOWN
+        self.selected_floors.discard(self.callbacks.current_floor)
+        if self.notional_direction is not None:
+            self.requested_floors[self.notional_direction].discard(self.callbacks.current_floor)
+            if self._should_move(self.notional_direction):
+                self.callbacks.motor_direction = self.notional_direction
+            else:
+                self._set_notional_direction_for_requests()
+                if self.notional_direction is not None:
+                    self.requested_floors[self.notional_direction].discard(self.callbacks.current_floor)
+                    if self._should_move(self.notional_direction):
+                        self.callbacks.motor_direction = self.notional_direction
+
